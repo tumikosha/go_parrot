906530ec6644a9ee7e7c7e07b757deed86d6c287
Tumikosha <tumikosha@gmail.com>
2020-07-01 13:44:19 +0300

    new version with YAML

--- importer/app.py
+++ importer/app.py
@@ -12,6 +12,7 @@ import argparse
 import logging
 from flask import Flask
 from flask_crontab import Crontab
+
 # configure logger
 logger = logging.getLogger('import_app')
 logger.setLevel(logging.INFO)
@@ -34,8 +35,13 @@ app = Flask(__name__)
 crontab = Crontab(app)  # Flask crontab
 
 
-def csv_2_df(path, dtype=None):
-	"""load csv to pandas dataframe """
+def csv_2_df(path, dtype=None) -> pd.DataFrame:
+	"""
+	load CSV to pandas dataframe
+	:param path: path to CSV file
+	:param dtype: list with DTYPES see read_csv description for details
+	:return: pd.DataFrame
+	"""
 	f = lambda s: datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
 	df_ = pd.read_csv(path, date_parser=f, dtype=dtype, parse_dates=['created_at', 'updated_at'])
 	df_['updated_at'] = pd.to_datetime(df_['updated_at'])
@@ -43,24 +49,32 @@ def csv_2_df(path, dtype=None):
 	return df_
 
 
-def load_all_files(dir, start_with):
-	order_files = [f for f in os.listdir(dir) if f.startswith(start_with)]
-	count = 0
-	for file in order_files:
-		orders_ = petl.fromcsv(dir + "/" + file)
-		if count == 0:
-			all = orders_
-		else:
-			all = petl.merge(all, orders_, key="updated_at")
-		count += 1
-	return all
+# def load_all_files(dir, start_with):
+# 	order_files = [f for f in os.listdir(dir) if f.startswith(start_with)]
+# 	count = 0
+# 	for file in order_files:
+# 		orders_ = petl.fromcsv(dir + "/" + file)
+# 		if count == 0:
+# 			all = orders_
+# 		else:
+# 			all = petl.merge(all, orders_, key="updated_at")
+# 		count += 1
+# 	return all
 
 
 def point_iterator(yaml_path, query, tip='csv_orders', return_type='row', start=db_util.VERY_EARLY_DATE,
 				   end=datetime.datetime.now()):
 	"""
 	Iterates over all points with specified type
+	:param yaml_path: path to YAML file
+	:param query:  TODO
+	:param tip:  point type to iterate [csv_orders, csv_users, orders_mongo_source, users_mongo_source, csv_dest, mongo_dest]
+	:param return_type:  ['point' | 'df' | 'row']
+	:param start:  start time  (WARNING do not supported for `point` and `row` iteration) TODO
+	:param end:  end time (WARNING do not supported for `point` and `row` iteration) TODO
+	:return: pd.DataFrame
 	"""
+
 	with open(yaml_path) as f:
 		config = yaml.safe_load(f)
 
@@ -113,8 +127,20 @@ def point_iterator(yaml_path, query, tip='csv_orders', return_type='row', start=
 						yield source_name_, source_, row
 
 
-def load_and_filter_all_users(cfg_path_, tip="csv_orders", start=db_util.VERY_EARLY_DATE,
-						end=datetime.datetime.now()) -> pd.DataFrame:
+def load_and_filter_all_users(cfg_path_, tip="csv_users", start=db_util.VERY_EARLY_DATE,
+							  end=datetime.datetime.now()) -> (pd.DataFrame, pd.DataFrame):
+	"""
+	Load YAML, Iterates over all points with specified type==tip as DataFrames
+	merge all iterated dataframes
+	converts datafields to `datetime` type
+	and makes GROUP BY
+	:param cfg_path_: path to YAML file
+	:param tip:  point type to iterate [csv_users ONLY!]
+	:param start:  start time  (WARNING do not supported for `point` and `row` iteration) TODO
+	:param end:  end time (WARNING do not supported for `point` and `row` iteration) TODO
+	:return: df_summ, df_summ_filtered - not_filterd and filtered dataframes with users from all source points
+	"""
+
 	df_summ = None
 	for source_name, source, df in point_iterator(cfg_path_, {}, tip=tip, return_type='df', start=start, end=end):
 		if df_summ is None:
@@ -131,7 +157,19 @@ def load_and_filter_all_users(cfg_path_, tip="csv_orders", start=db_util.VERY_EA
 
 
 def load_and_filter_all_orders(cfg_path_, tip="csv_orders", start=db_util.VERY_EARLY_DATE,
-						end=datetime.datetime.now()) -> pd.DataFrame:
+							   end=datetime.datetime.now()) -> (pd.DataFrame, pd.DataFrame):
+	"""
+	Load YAML, Iterates over all points with specified type==tip as DataFrames
+	merge all iterated dataframes
+	converts datafields to `datetime` type
+	and makes GROUP BY
+	:param cfg_path_: path to YAML file
+	:param tip:  point type to iterate [csv_orders ONLY]
+	:param start:  start time  (WARNING do not supported for `point` and `row` iteration) TODO
+	:param end:  end time (WARNING do not supported for `point` and `row` iteration) TODO
+	:return: df_summ, df_summ_filtered - not_filterd and filtered dataframes with users from all source points
+	"""
+
 	df_summ = None
 	for source_name, source, df in point_iterator(cfg_path_, {}, tip=tip, return_type='df', start=start, end=end):
 		if df_summ is None:
@@ -145,33 +183,39 @@ def load_and_filter_all_orders(cfg_path_, tip="csv_orders", start=db_util.VERY_E
 		df_summ_filtered = df_summ.loc[df_summ.groupby("id")["updated_at"].idxmax()]  # keep max updated_at
 	else:
 		df_summ_filtered = None
-	# print(df_summ)
-	# print(df_summ_filtered)
 	return df_summ, df_summ_filtered
 
-#
-# def replace_value(d, func):
-# 	for key in d.keys():
-# 		d[key] = func(d[key])
-# 	return d
 
-
-def NaT(df, column):
-	"""drop records with NaT values in column"""
+def NaT(df, column) -> pd.DataFrame:
+	"""
+	Drop records with NaT values in column
+	:param df: path to YAML file
+	:param column:  point type to iterate [csv_orders ONLY]
+	:return: pd.DataFrame
+	"""
 	df[[column]] = df[[column]].astype(object).where(
 		df[[column]].notnull(), None)
-
-	# df[column] = df[column].astype(str)
-	# df[column] = df[column].apply(lambda x : None if x=="NaT" else x)
 	return df
 
 
 def df_to_mongo(df, uri, db_name, collection_name, point, desc="write to mongo",
 				etl=lambda record: record,  # default: no etl
-				func=db_util.update_record):
+				func=db_util.update_record) -> None:
+	"""
+	write dataframe to mongodb collection
+	:param df: pandas.DataFrame
+	:param uri:  mongpdb uri
+	:param db_name:
+	:param collection_name:
+	:param point: dict, sometimes need parameters for this point from YAMl file
+	:param desc:
+	:param etl: ROW transformation function
+	:param func: function for update record in db
+	:return:  None
+	"""
+
 	client = pymongo.MongoClient(uri)
 	db = client[db_name]
-
 	with tqdm.tqdm(total=len(df), desc=desc) as pbar:
 		for idx, row_ in df.T.iteritems():
 			# record = dict(row_)
@@ -182,15 +226,27 @@ def df_to_mongo(df, uri, db_name, collection_name, point, desc="write to mongo",
 			pbar.update(1)
 
 
-def merge_df(df1, df2):
-	if (df1 is None) and (df2 is None):    return None
+def merge_df(df1, df2) -> pd.DataFrame:
+	"""merge 2 dataframes with None control
+	:param df1: pd.dataFrame
+	:param df2: pd.dataFrame
+	:return:  result pd.DataFrame or None
+	"""
+	if (df1 is None) and (df2 is None):
+		return None
 	if df1 is None: return df2
 	if df2 is None: return df1
 	df_ = pd.concat([df1, df2], ignore_index=True)
 	return df_
 
 
-def fix_df_users(df_users):
+def fix_df_users(df_users) -> pd.DataFrame:
+	"""
+	Fix wrong columns formats
+	:param df_users: pd.dataFrame
+	:return:  result pd.DataFrame
+	"""
+
 	if df_users is not None:
 		df_users['user_created_at'] = df_users['created_at']
 		df_users['user_updated_at'] = df_users['updated_at']
@@ -200,6 +256,20 @@ def fix_df_users(df_users):
 
 # def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=datetime.datetime.now()) -> int:
 def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("1 day in")) -> int:
+	"""
+	Life cycle function:
+		Step1. Loads orders and users from different sources(aka points)
+		Step2. Filter & grouping
+		Step3. Join  Orders and Users
+		Step4. write results to destination points
+
+	:param str cfg_path: path to YAML file
+	:param datetime.datetime start:  begin time
+	:param datetime.datetime end: end time
+	:return:  number of produced full_orders
+	:rtype: int
+	"""
+	# STEP1. loading & Filtering
 	print('processing', cfg_path)
 	print('Loading CSV users...')
 	df_users_unfiltered, df_users = load_and_filter_all_users(cfg_path, tip="csv_users")
@@ -208,10 +278,10 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 	df_orders_unfiltered, df_orders = load_and_filter_all_orders(cfg_path, tip="csv_orders")
 
 	print('Loading Mongo orders...')
-	df_orders_unfiltered_M, df_orders_M = load_and_filter_all_orders(cfg_path, tip="orders_mongo_source", start=start, end=end)
+	df_orders_unfiltered_M, df_orders_M = load_and_filter_all_orders(cfg_path, tip="orders_mongo_source", start=start,
+																	 end=end)
 
 	print('Loading Mongo users...')  # we ignore time range for users!
-	# df_users_unfiltered_M, df_users_M = load_and_filter_all(cfg_path, tip="users_mongo_source", start=start, end=end)
 	df_users_unfiltered_M, df_users_M = load_and_filter_all_users(cfg_path, tip="users_mongo_source")
 	df_users = fix_df_users(df_users)
 	df_users_M = fix_df_users(df_users_M)
@@ -229,7 +299,7 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 
 	# print(df_users)
 
-	# join....
+	# Step 3.GROUP &  JOIN....
 	print('Join orders and users...')
 	if df_users is None:
 		print("No users...")
@@ -241,12 +311,11 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 		print("Orders:", len(df_orders))
 
 	if (df_users is not None) and (df_orders is not None):  # group and join
+		# GROUP
 		df_orders = df_orders.loc[df_orders.groupby("id")["updated_at"].idxmax()]  # keep max updated_at
 		df_users = df_users.loc[df_users.groupby("user_id")["user_updated_at"].idxmax()]  # keep max updated_at
+		# JOIN
 		full_orders = df_orders.join(df_users.set_index('user_id'), on='user_id')
-		if len(df_orders) != len(full_orders):
-			print("HALTED", len(df_orders), len(full_orders))
-			sys.exit()
 	else:
 		full_orders = None
 
@@ -255,55 +324,50 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 	if full_orders is None:
 		print("No full_orders to proceed...")
 		return 0
+
+	# STEP 4. Saving results
 	# --- === It can be Only One CSV destination ===---
 	print('Saving  CSV destinations...')
 	if full_orders is not None:
 		full_orders.to_csv("data/full_orders.csv", na_rep='None')
-	# full_orders.to_csv("data/full_orders.csv", na_rep=None)
 
 	for source_name, source, point_type in \
 			point_iterator(cfg_path, {}, tip="mongo_dest", return_type='point'):
-		# print(source_name, source['uri'])
 		full_orders[['updated_at']] = full_orders[['updated_at']].astype(object).where(
 			full_orders[['updated_at']].notnull(), None)
-
-		# full_orders = NaT(full_orders, 'updated_at')
-		# full_orders = NaT(full_orders, 'created_at')
-		# full_orders = NaT(full_orders, 'user_created_at') # TODO
-		# full_orders = NaT(full_orders, 'user_updated_at') # TODO
-
+		# ERASING POINT TODO for all collections
 		if source.get('erase_point_on_start', None) is not None:
 			if source['erase_point_on_start'] == True:
 				db_util.clear_point(source)
 
+		# SAVE full_orders
 		full_orders['_id'] = full_orders['id']
 		df_to_mongo(full_orders, source['uri'], source['db_name'], source['table_name'], source,
 					func=db_util.update_full_order,
 					desc="write `full_orders` to mongo " + source_name)
 
+		# save FILTERED ORDERS
 		filtered_orders_table_name = source.get('filtered_orders_table_name', None)
 		if filtered_orders_table_name is not None:
-			# df_orders['_id'] = df_orders['user_id']
 			df_orders['_id'] = df_orders['id']
 			df_to_mongo(df_orders, source['uri'], source['db_name'], filtered_orders_table_name, source,
 						func=db_util.update_order,
 						desc="write `" + filtered_orders_table_name + "` to mongo " + source_name)
-
+		# SAVE filtered users
 		filtered_users_table_name = source.get('filtered_users_table_name', None)
 		if filtered_users_table_name is not None:
 			df_users['_id'] = df_users['user_id']
 			df_users['created_at'] = df_users['user_created_at']
-			df_users['updated_at'] = df_users['user_updated_at'] # TODO DROP
+			df_users['updated_at'] = df_users['user_updated_at']  # TODO DROP
 			df_users = df_users.drop(['user_updated_at'], axis=1)
 			df_users = df_users.drop(['user_created_at'], axis=1)
 
 			df_to_mongo(df_users, source['uri'], source['db_name'], filtered_users_table_name, source,
 						func=db_util.update_user,
 						desc="write `users` to mongo " + source_name)
-
+		# SAVE unfltered orders
 		unfiltered_orders_table_name = source.get('unfiltered_orders_table_name', None)
 		if unfiltered_orders_table_name is not None:
-			# df_orders_unfiltered['_id'] = df_orders_unfiltered['user_id'] + "==" + df_orders_unfiltered[
 			df_orders_unfiltered['_id'] = df_orders_unfiltered['id'] + "==" + df_orders_unfiltered[
 				'updated_at'].astype(str)
 			df_orders_unfiltered = NaT(df_orders_unfiltered, 'updated_at')
@@ -312,16 +376,13 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 						# func=db_util.update_record,
 						func=db_util.update_record,
 						desc="write `" + unfiltered_orders_table_name + "` to mongo " + source_name)
-
+		# SAVE unfltered users
 		unfiltered_users_table_name = source.get('unfiltered_users_table_name', None)
 		if unfiltered_orders_table_name is not None:
 			df_users_unfiltered['_id'] = df_users_unfiltered['user_id'] + "==" + df_users_unfiltered[
 				'updated_at'].astype(str)
 			df_users_unfiltered = NaT(df_users_unfiltered, 'updated_at')
 			df_users_unfiltered = NaT(df_users_unfiltered, 'created_at')
-			# df_users_unfiltered = NaT(df_users_unfiltered, 'user_created_at')
-			# df_users_unfiltered = NaT(df_users_unfiltered, 'user_updated_at')
-
 			df_to_mongo(df_users_unfiltered, source['uri'], source['db_name'], unfiltered_users_table_name, source,
 						func=db_util.update_record,
 						desc="write `" + unfiltered_users_table_name + "` to mongo " + source_name)
@@ -329,6 +390,7 @@ def process_yaml(cfg_path, start=db_util.VERY_EARLY_DATE, end=dateparser.parse("
 		return len(full_orders)
 	else:
 		return 0
+
 
 # */5 * * * *   At every 5th minute
 @crontab.job(minute="*/5")
@@ -344,17 +406,17 @@ def scheduled_job(  # start_moment=dateparser.parse('5 min ago'),
 	logger.info('flask cronjob started')
 	try:
 		now = datetime.datetime.now()
-		moment =  dateparser.parse("7 days ago 00:00")
+		moment = dateparser.parse("7 days ago 00:00")
 		moment_next = dateparser.parse("2 days in 00:00")
 		# scheduled_job(end_moment=moment)
 		logger.info("Period: " + str(moment) + " ::: " + str(moment_next))
 		num = process_yaml('cron.yaml', start=moment, end=moment_next)
 		logger.info("orders imported: " + str(num))
-		# logger.info("customers imported: " + str(total_customers2 - total_customers))
-		# logger.info("empty added: " + str(total_empty2 - total_empty))
+	# logger.info("customers imported: " + str(total_customers2 - total_customers))
+	# logger.info("empty added: " + str(total_empty2 - total_empty))
 
 	except Exception as e:
-		logger.error('scheduled_job: '+str(e))
+		logger.error('scheduled_job: ' + str(e))
 
 	logger.info('flask cronjob ended')
 
@@ -381,10 +443,10 @@ if __name__ == "__main__":
 	parser.add_argument('--erase', dest='erase', action='store_true', default=False)
 
 	args = parser.parse_args()
-	start = dateparser.parse(args.start.replace("_"," "))
-	end = dateparser.parse(args.end.replace("_"," "))
+	start = dateparser.parse(args.start.replace("_", " "))
+	end = dateparser.parse(args.end.replace("_", " "))
 	print("Period:", start, end)
-	logger.info("Period: " + str(start)+" ::: "+str(end))
+	logger.info("Period: " + str(start) + " ::: " + str(end))
 	# process_yaml(args.yaml, start=start, end=end)
 	if args.mode == 'all':  # 'simulate'
 		# process_yaml("step_1.yaml")
